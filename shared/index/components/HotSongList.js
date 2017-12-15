5383412e1124752b4d9ac757fb41ab68f1e5bddc
Puxuan He <puxuanhe@Puxuans-MacBook-Pro.local>
2017-12-14 22:37:50 -0600

    update index ui

--- shared/index/components/HotSongList.js
+++ shared/index/components/HotSongList.js
@@ -5,18 +5,26 @@ export default function ({ songs, title }) {
   return (
     <div className={styles.hotSongs}>
       { !!songs.length && <div className={styles.title}>{title}</div> }
-      {
-        songs.map(({
-         id, songName, singerName, albumId,
-       }) => (
-         <div key={id}>
-           <a href={`/song/${songName}?singer=${singerName}&songId=${id}&imageId=${albumId}`}>
-             <span>{songName}</span>&nbsp;-&nbsp;
-             <span className={styles.singerName}>{singerName}</span>
-           </a>
-         </div>
-       ))
-      }
+      <div className={styles.srollable}>
+        {
+          songs.slice(0, 10).map(({
+           id, songName, singerName, albumId,
+         }) => {
+           const url = `http://imgcache.qq.com/music/photo/album_300/${albumId % 100}/300_albumpic_${albumId}_0.jpg`;
+           return (
+             <div key={id}>
+               <a href={`/song/${songName}?singer=${singerName}&songId=${id}&imageId=${albumId}`}>
+                 <img width="100%" src={url} alt="" />
+                 <div>
+                   <div>{songName}</div>
+                   <div className={styles.singerName}>{singerName}</div>
+                 </div>
+               </a>
+             </div>
+           );
+         })
+        }
+      </div>
     </div>
   );
 }
