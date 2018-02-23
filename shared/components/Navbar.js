509ca77473426b8b70f8c0dc84b52d04f8011073
Puxuan He <puxuanhe@Puxuans-MacBook-Pro.local>
2018-02-22 22:49:29 -0600

    update animation

--- shared/components/Navbar.js
+++ shared/components/Navbar.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { inject, observer } from 'mobx-react';
 import MyAccount from './myAccount';
 import styles from './navbar.scss';
 
-function Navbar({ store: { title, isIndex, showNav } }) {
-  if (!showNav) {
-    return null;
-  }
+export default function () {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.navInner}>
-        {
-          isIndex ? (
-            <div className={styles.title}>
-              <div>
-                <h1>海外音悦台</h1>
-                <div>
-                  <MyAccount />
-                </div>
-              </div>
-            </div>
-          ) : (
-            <React.Fragment>
-              <div>
-                <Link to="/">
-                  <i className="material-icons">keyboard_arrow_left</i>
-                </Link>
-              </div>
-              <div className={styles.pagetitle}>{title}</div>
-              <MyAccount />
-            </React.Fragment>
-          )
-        }
+    <div className={styles.navBar}>
+      <div className={styles.title}>
+        <div>
+          <h1>海外音悦台</h1>
+          <div>
+            <MyAccount />
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-export default inject('store')(observer(Navbar));
