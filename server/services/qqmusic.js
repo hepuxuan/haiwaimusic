349357e6a758d59fe2d4e7ee913c58ec1a460131
Puxuan He <puxuanhe@Puxuans-MacBook-Pro.local>
2018-06-14 23:03:54 -0500

    fix songs

--- server/services/qqmusic.js
+++ server/services/qqmusic.js
@@ -2,6 +2,7 @@ const fetch = require('isomorphic-fetch');
 const { parseJsonP } = require('../../shared/utils');
 const { client, getAsync } = require('./redis');
 const he = require('he');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const ONE_DAY = 60 * 60 * 24;
 
@@ -99,13 +100,20 @@ module.exports = {
       }
       const t = (new Date).getUTCMilliseconds(); // eslint-disable-line
       const guid = (Math.round(2147483647 * Math.random()) * t) % 1e10;
-      const fileName = `C200${mid}.m4a`;
-      const url = `http://base.music.qq.com/fcgi-bin/fcg_musicexpress.fcg?json=3&guid=${guid}&g_tk=938407465&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf8&platform=yqq&jsonpCallback=&needNewCode=0`;
-      return fetch(url)
+      const fileName = `C400${mid}.m4a`;
+      const url = `https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?uin=0&g_tk=1278911659&loginUin=0&hostUin=0&inCharset=utf8&outCharset=utf-8&jsonpCallback=MusicJsonCallback&notice=0&platform=yqq&needNewCode=0&cid=205361747&uin=0&songmid=${mid}4&filename=${fileName}&guid=${guid}`;
+      return fetch(url, {
+        agent: new HttpsProxyAgent('http://58.221.72.189:3128'),
+        headers: {
+          Referer: 'https://y.qq.com/portal/player.html',
+          Host: 'y.qq.com',
+          Origin: 'https://y.qq.com',
+        },
+      })
         .then(res => res.json())
-        .then(({ key }) => key)
+        .then(res => res.data.items[0].vkey)
         .then((vkey) => {
-          const address = `https://dl.stream.qqmusic.qq.com/${fileName}?vkey=${vkey}&guid=${guid}&fromtag=52`;
+          const address = `https://dl.stream.qqmusic.qq.com/${fileName}?vkey=${vkey}&guid=${guid}&uin=0&fromtag=66`;
           client.set(`song.address:${mid}`, address, 'EX', ONE_DAY);
           return address;
         });
